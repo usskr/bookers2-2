779f50870331c7d60a5826f4878597fae59d1991
usskr <rei.takagi731@gmail.com>
2021-08-30 07:58:28 +0000

    [Add]bookのshowページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -16,10 +17,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+
   end
 
   def destroy
-
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
