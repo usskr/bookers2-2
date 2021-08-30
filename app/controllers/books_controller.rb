9f323e1aa0ad6d3101911ec4e79aeb305f367715
usskr <rei.takagi731@gmail.com>
2021-08-30 12:48:57 +0000

    [Add]バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice:'You have created book successfully.'
     else
+      @books = Book.all
       render :index
     end
   end
@@ -28,7 +29,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice:'You have updated book successfully.'
     else
-      render :show
+      render :edit
     end
   end
 
