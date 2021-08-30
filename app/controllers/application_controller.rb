e64c2c7224ed8bf7423836c841dac87e975bce74
usskr <rei.takagi731@gmail.com>
2021-08-30 04:44:06 +0000

    [Add]log inページの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
-  
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
 end
