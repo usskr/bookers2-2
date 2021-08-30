70130e2673972c23d72d85a9c02f978f7b635e47
usskr <rei.takagi731@gmail.com>
2021-08-30 07:18:37 +0000

    [Add]bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,31 @@
 class BooksController < ApplicationController
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
   def edit
   end
+
+  def destroy
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
