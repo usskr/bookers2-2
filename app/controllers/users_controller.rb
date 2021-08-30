db74c8acce571573990242e8dd7592f4cfc9eb1e
usskr <rei.takagi731@gmail.com>
2021-08-30 11:49:08 +0000

    [Add]userのeditページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def index
     @users = User.all
   end
@@ -10,6 +10,19 @@ class UsersController < ApplicationController
   end
 
   def edit
-    
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user  = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c04a79b9027f1c7478d53f98dcaa23ca254d51e823e843bc679e4e148fb558eb44a6f54c5da2186aaf9b097e6a9c4d7e6ae33bf47795d78fd772cf45a120f56'
