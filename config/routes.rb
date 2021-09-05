21ee935b0fddf855dcf96e51ae734f5ef9108f79
usskr <rei.takagi731@gmail.com>
2021-09-05 08:40:21 +0000

    [Add] いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => "homes#about"
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :edit, :update]
 end
