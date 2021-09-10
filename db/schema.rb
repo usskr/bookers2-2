b6617926ad74ea500ffd13bc29dfdb5a6b2c201d
usskr <rei.takagi731@gmail.com>
2021-09-10 05:14:49 +0000

    [応用課題7d] 5段階評価（投稿、本の一覧、本の詳細、ユーザーの詳細画面）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_06_111654) do
+ActiveRecord::Schema.define(version: 2021_09_10_030608) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_09_06_111654) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "rate"
   end
 
   create_table "favorites", force: :cascade do |t|
