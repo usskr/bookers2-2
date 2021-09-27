fabc2e154b86297ff5bbf655780162c92708431e
usskr <rei.takagi731@gmail.com>
2021-09-27 01:47:57 +0000

    [応用課題9d] 投稿時のカテゴリタグ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,7 +48,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :rate)
+    params.require(:book).permit(:title, :body, :category, :rate)
   end
 
   def ensure_correct_user
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  belongs_to :category
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
